bf4719ed1a0e26a0f69a5cc630e3137d41d2e981
Owen Coogan <owencoogan01@gmail.com>
2022-10-25 15:43:08 +0200

    added stats

--- controllers/comments/comments.controller.js
+++ controllers/comments/comments.controller.js
@@ -3,7 +3,9 @@ const { Comment } = require('../../models');
 const createOne = async (req, res) => {
   const { content, userId,postId } = req.body;
   console.log(req.body);
-
+  if (!content) {
+    return res.status(400).json({ message: 'content is required' });
+  }
   const newComment = await Comment.create({
     content: content,
     userId: userId,
