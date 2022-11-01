4893f8748cb4154c2f4283ff25f289c0dc301e6c
Owen Coogan <owencoogan01@gmail.com>
2022-11-01 23:39:45 +0100

    added stats

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-npx sequelize db:create
-npx sequelize db:migrate
-npx sequelize db:seed:all
+npx sequelize db:create --env production
+npx sequelize db:migrate --env production
+npx sequelize db:seed:all --env production
 nodemon server.js
