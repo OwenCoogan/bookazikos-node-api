c2ef99c7f50ba4ae11f3da4f0b05e4c747e8081f
Owen Coogan <owencoogan01@gmail.com>
2023-03-05 22:25:46 +0100

    added fixes docker

--- seeders/20220409191111-User.js
+++ seeders/20220409191111-User.js
@@ -1,31 +1,44 @@
 'use strict';
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
+
+const Users = [{
+  id:uuidv4(),
+  firstName: 'Jessica',
+  lastName: 'Narvaez',
+  email: 'jnarvaezg@outlook.com',
+  role: 'admin',
+  password: await bcrypt.hash('hello', 10),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+},
+{
+  id:uuidv4(),
+  firstName: 'Owen',
+  lastName: 'Coogan',
+  email: 'owencoogan01@gmail.com',
+  role: 'super-admin',
+  password: await bcrypt.hash('hello', 10),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+]
+
+
+
 module.exports = {
 
   async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('Users', [
-      {
-        id:uuidv4(),
-        firstName: 'Jessica',
-        lastName: 'Narvaez',
-        email: 'jnarvaezg@outlook.com',
-        role: 'admin',
-        password: await bcrypt.hash('hello', 10),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id:uuidv4(),
-        firstName: 'Owen',
-        lastName: 'Coogan',
-        email: 'owencoogan01@gmail.com',
-        role: 'admin',
-        password: await bcrypt.hash('hello', 10),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }
-    ], {});
+        Users.forEach(user => {
+          User.findOne({ where: { email: user.email } })
+            .then((user) => {
+              if (!user) {
+                User.create(user);
+              }
+            })
+        })
+      ])
   },
 
   async down (queryInterface, Sequelize) {
