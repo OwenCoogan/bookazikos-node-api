f1977ab85bf759776359bde4caae2c849ce35101
Owen Coogan <owencoogan01@gmail.com>
2023-03-07 17:54:17 +0100

    added fixes docker

--- seeders/20220409191111-User.js
+++ seeders/20220409191111-User.js
@@ -2,34 +2,32 @@
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
 
-const Users = [{
-  id:uuidv4(),
-  firstName: 'Jessica',
-  lastName: 'Narvaez',
-  email: 'jnarvaezg@outlook.com',
-  role: 'admin',
-  password: await bcrypt.hash('hello', 10),
-  createdAt: new Date(),
-  updatedAt: new Date(),
-},
-{
-  id:uuidv4(),
-  firstName: 'Owen',
-  lastName: 'Coogan',
-  email: 'owencoogan01@gmail.com',
-  role: 'super-admin',
-  password: await bcrypt.hash('hello', 10),
-  createdAt: new Date(),
-  updatedAt: new Date(),
-}
-]
 
 
 
 module.exports = {
 
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', Users, {});
+    await queryInterface.bulkInsert('Users', [{
+      id:uuidv4(),
+      firstName: 'Jessica',
+      lastName: 'Narvaez',
+      email: 'jnarvaezg@outlook.com',
+      role: 'admin',
+      password: await bcrypt.hash('hello', 10),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id:uuidv4(),
+      firstName: 'Owen',
+      lastName: 'Coogan',
+      email: 'owencoogan01@gmail.com',
+      role: 'super-admin',
+      password: await bcrypt.hash('hello', 10),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
   },
 
   async down (queryInterface, Sequelize) {
